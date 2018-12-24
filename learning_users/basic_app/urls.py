e1c6c5c8ce28421478fe8689a6d06895489b6fc3
Alexandros Pitteros <alexpit@192.168.2.8>
2018-12-24 16:32:39 +0200

    first

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from basic_app import views
+
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('register/', views.user_register, name = 'register'),
+    path('user_login/', views.user_login, name = 'user_login'),
+    path('user_logout', views.user_logout, name = 'user_logout'),
+]
